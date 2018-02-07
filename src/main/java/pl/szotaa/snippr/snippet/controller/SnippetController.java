c2d651e347e88307003866d8db2e61e1be381d9a
szotaa <shoothas@gmail.com>
2018-02-07 14:51:02 +0100

    add initial CRUD methods

--- src/main/java/pl/szotaa/snippr/snippet/controller/SnippetController.java
+++ src/main/java/pl/szotaa/snippr/snippet/controller/SnippetController.java
@@ -2,12 +2,49 @@ package pl.szotaa.snippr.snippet.controller;
 
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import pl.szotaa.snippr.snippet.domain.Snippet;
 import pl.szotaa.snippr.snippet.service.SnippetService;
+
+import java.util.List;
+
+//TODO: validation
 
 @RestController
 @AllArgsConstructor(onConstructor = @__({@Autowired}))
+@RequestMapping("/snippet")
 public class SnippetController {
 
     private final SnippetService snippetService;
+
+    @PostMapping
+    public ResponseEntity add(@RequestBody Snippet snippet){
+        //TODO: add logic
+        return null;
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Snippet> getById(@PathVariable Long id){
+        //TODO: getById logic
+        return null;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Snippet>> getAll(){
+        //TODO: getAll logic
+        return null;
+    }
+
+    @PatchMapping("/{id}")
+    public ResponseEntity updateExisting(@PathVariable Long id){
+        //TODO: updateExisting logic
+        return null;
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity deleteExisting(@PathVariable Long id){
+        //TODO: deleteExisting logic
+        return null;
+    }
 }
