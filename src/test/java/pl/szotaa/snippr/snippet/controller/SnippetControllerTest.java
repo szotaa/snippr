4fb16b337cf94020ff007852d2a220d577891598
szotaa <shoothas@gmail.com>
2018-02-10 12:57:17 +0100

    mock user in test

--- src/test/java/pl/szotaa/snippr/snippet/controller/SnippetControllerTest.java
+++ src/test/java/pl/szotaa/snippr/snippet/controller/SnippetControllerTest.java
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -18,6 +19,7 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import pl.szotaa.snippr.snippet.domain.Snippet;
 import pl.szotaa.snippr.snippet.service.SnippetService;
 
+@WithMockUser
 @RunWith(SpringRunner.class)
 @WebMvcTest(SnippetController.class)
 public class SnippetControllerTest {
