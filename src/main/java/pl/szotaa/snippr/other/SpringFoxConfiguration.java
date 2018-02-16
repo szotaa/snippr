87e98edd1b49b78b325d7b6e75198dae5d560b2e
szotaa <shoothas@gmail.com>
2018-02-16 13:23:55 +0100

    dont show admin-only endpoints in api-docs

--- src/main/java/pl/szotaa/snippr/other/SpringFoxConfiguration.java
+++ src/main/java/pl/szotaa/snippr/other/SpringFoxConfiguration.java
@@ -16,7 +16,7 @@ public class SpringFoxConfiguration {
     public Docket apiDocket(){
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.any())
+                .apis(RequestHandlerSelectors.basePackage("pl.szotaa.snippr"))
                 .paths(PathSelectors.any())
                 .build();
     }
