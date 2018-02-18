c5c1f180ed1a6bdb58bb04c84055464c81675b6b
szotaa <shoothas@gmail.com>
2018-02-18 21:56:01 +0100

    initial test suite running all tests

--- src/test/java/pl/szotaa/snippr/SnipprApplicationTests.java
+++ src/test/java/pl/szotaa/snippr/SnipprApplicationTests.java
@@ -2,11 +2,17 @@ package pl.szotaa.snippr;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.junit.runners.Suite;
+import pl.szotaa.snippr.snippet.domain.SnippetJsonTest;
+import pl.szotaa.snippr.snippet.validation.NotBeforeCurrentTimeValidatorTest;
+import pl.szotaa.snippr.snippet.validation.ProgrammingLanguageValidatorTest;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+		SnippetJsonTest.class,
+		NotBeforeCurrentTimeValidatorTest.class,
+		ProgrammingLanguageValidatorTest.class
+})
 public class SnipprApplicationTests {
 
 	@Test
