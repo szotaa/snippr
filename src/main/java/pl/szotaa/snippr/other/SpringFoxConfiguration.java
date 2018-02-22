49a3761d3fd1b1356e19bfa61fca44dc1e52a188
szotaa <shoothas@gmail.com>
2018-02-22 12:17:34 +0100

    comment out profile

--- src/main/java/pl/szotaa/snippr/other/SpringFoxConfiguration.java
+++ src/main/java/pl/szotaa/snippr/other/SpringFoxConfiguration.java
@@ -11,7 +11,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 @EnableSwagger2
-//@Profile(value = {"swagger"})
+@Profile(value = {"swagger"})
 public class SpringFoxConfiguration {
 
     @Bean
